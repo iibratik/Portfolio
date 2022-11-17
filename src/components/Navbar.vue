4a2fa120a1055f4eba1fa48d03ae277c942898a0
iibratik <iibragimov2417@gmail.com>
2022-11-17 05:51:31 +0500

    adaptive

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,10 @@
 <template lang="pug">
 nav.navbar
-    .container.navbar-content
+    .container.navbar-content.desctop-nav
         router-link(to='/')
             img(src="@/assets/img/Logo-main.svg")
+        button.burger-menu(@click="navbar = !navbar")
+            <i class="fas fa-bars"></i>
         ul.navbar__lists
             li.navbar__list
                 router-link.navbar__link(to="/") Home
@@ -16,13 +18,30 @@ nav.navbar
               icon-btn(@click="switchTheme" v-show="!themeVar")
                   i.fas.fa-sun
               white-btn(@click="$router.push('/contacts')") Let's talk
+    transition(name="mobile-nav-trans")
+      .navbar-content.mobile-nav(v-if="navbar")
+            ul.navbar__lists
+                    li.navbar__list
+                        router-link.navbar__link(to="/") Home
+                    li.navbar__list
+                        router-link.navbar__link(to="/about") About
+                    li.navbar__list
+                        router-link.navbar__link(to="/contact") Contact
+            .navbar__btns
+                    icon-btn(@click="switchTheme" v-show="themeVar")
+                        i.fas.fa-moon
+                    icon-btn(@click="switchTheme" v-show="!themeVar")
+                        i.fas.fa-sun
+                    white-btn(@click="$router.push('/contacts')") Let's talk
 </template>
 
 <script>
-export default{
+export default {
   data() {
     return {
       themeVar: true,
+      navbar: false,
+      mobileNav: "",
     }
   },
   methods: {
@@ -31,15 +50,15 @@ export default{
       const navbar = document.querySelector('.navbar')
       navbarLinks.forEach((link) => {
         if (window.scrollY >= 10) {
-          navbar.style.filter = "drop-shadow(20px 10px 12px rgba(0, 0, 0, 0.3))"
+          navbar.style.filter = 'drop-shadow(20px 10px 12px rgba(0, 0, 0, 0.3))'
           link.style.color = 'var(--black)'
           navbar.style.background = 'var(--white)'
           navbar.style.position = 'fixed'
-          navbar.style.top = "0"
-          navbar.style.zIndex = "999"
-          navbar.style.width = "100%"
+          navbar.style.top = '0'
+          navbar.style.zIndex = '999'
+          navbar.style.width = '100%'
         } else {
-          navbar.style.filter = "none "
+          navbar.style.filter = 'none '
           link.style.color = 'var(--black)'
           navbar.style.transform = 'translateX(0)'
           navbar.style.left = '0'
